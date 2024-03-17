7f283b87faef1a95a0be8915b778a64f25cb7168
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-03-17 13:51:37 +0545

    Implimented server side validation

--- app.py
+++ app.py
@@ -15,42 +15,57 @@ app.secret_key = 'your_secret_key'
 
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        users = load_users()
+    username = request.form['username']
+    password = request.form['password']
+    users = load_users()
 
-        if username in users and bcrypt.check_password_hash(users[username], password):
-            session['username'] = username
-
-            return redirect(url_for('dashboard'))
-        else:
-            return 'Invalid username or password'
-
-    return render_template('login.html')
+    if username in users and bcrypt.check_password_hash(users[username], password):
+        session['username'] = username
+        return redirect(url_for('dashboard'))
+    else:
+        return 'Invalid username or password <button @click="login = true" class="bg-neutral-400 hover:bg-neutral-300 px-4 py-2 text-neutral-900 rounded-md w-fit" >Back to Login</button> '
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    username = request.form['username']
+    password = request.form['password']
 
-        # Load existing users
-        users = load_users()
-        # Check if the username already exists
-        if username in users:
-            return 'Username already exists', 400
+    # Load existing users
+    users = load_users()
+    # Check if the username already exists
+    if username in users:
+        return 'Username already exists  '
+    password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+    # Check if the username already existsnodes=graph_manager.nodes.values(), graph_data=graphdata
+    new_user = User(username, password_hash)
+    save_user(new_user)
 
-        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-        # Check if the username already existsnodes=graph_manager.nodes.values(), graph_data=graphdata
-        new_user = User(username, password_hash)
-        save_user(new_user)
-        return 'User Created Successfully' 
+    return 'User Created Successfully <button @click="login = true" class="bg-neutral-400 hover:bg-neutral-300 px-4 py-2 text-neutral-900 rounded-md w-fit" >Back to Login</button>' 
 
-    #return render_template('register.html')
+# Register form Validation Starts ------------------------------
+@app.route('/username-validation', methods=['POST'])
+def userValidation():
+    username = request.form['username']
+    # Load existing users
+    users = load_users()
+    if len(username) < 3:
+        return 'Username cannot be less then 3 characters'
+    # Check if the username already exists
+    if username in users:
+        return 'Username already exists'
+
+    return ""
+
+@app.route('/user-password-validation', methods=['POST'])
+def userPasswordValidation():
+    password = request.form['password']
+    if len(password) < 8:
+        return 'password cannot be less then 8 characters'
+    return ""
+# Register Form Validation Ends ------------------------------
 
 
 @app.route('/logout')
