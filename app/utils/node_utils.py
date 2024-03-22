8b7d27ef7a5be5c9a0185eec654aba0058fbdcb9
Aaditya Jha <45432533+Aadityaza@users.noreply.github.com>
2024-03-22 22:14:45 +0545

    content llinked and minor fix

--- app/utils/node_utils.py
+++ app/utils/node_utils.py
@@ -15,3 +15,12 @@ def map_score_to_radius(score, min_radius, max_radius):
     radius = min_radius + (max_radius - min_radius) * score
     
     return radius
+
+def get_content_by_id(data, target_id):
+    # Iterate through nodes to find the node with the matching ID
+    for node in data['nodes']:
+        if node['id'] == target_id:
+            return node['content']
+    
+    # If no matching ID found
+    return None
